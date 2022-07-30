e07541489f6b4539988c1134915e595292c7466a
Alejandro Sandí <alesf22@gmail.com>
2022-07-30 17:42:04 -0600

    fixed style details

--- src/components/contacts/contactsList/index.tsx
+++ src/components/contacts/contactsList/index.tsx
@@ -18,6 +18,7 @@ export default function ContactsList({ contacts }: { contacts: UserType[] }) {
           width: 100%;
           height: calc(100% - 70px);
           overflow-y: auto;
+          overflow-x: hidden;
           gap: 10px;
         }
       `}</style>
